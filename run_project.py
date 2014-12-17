928f30be7475a5b5a714378ac6ef344b273f8f24
Uriel Mandujano <umanduj2@cs.swarthmore.edu>
2014-12-17 13:18:34 -0500

    adding pipeline file

---
+++ run_project.py
@@ -0,0 +1,39 @@
+"""
+Uriel Mandujano
+Alpha Chau
+CS68 BioInformatics
+"""
+
+from parser import *
+from classes import *
+
+# In this file we run the ANN unsing the most predictive genes
+# stored in predictiveGenes.txt.
+
+def main():
+    NUM_GENES = 100
+
+    nn = Network(1, NUM_GENES )
+
+    cases = readDataFile()
+    recurrence = readDemoFile()
+    pred_genes = readPredictiveGenes( NUM_GENES )
+
+    # data[patient] = [survival, [ list of gene expressions] ]
+    data = combineData(cases, recurrence)
+
+    # Train the Neural Net
+    for patient in data:
+        survival = data[patient][0]        
+        expression = data[patient][1]
+        use_genes  = []
+
+        for g in pred_genes:
+            use_genes.append( float(expression[g]) )
+        
+        nn.train(use_genes, survival)
+    
+    # Evaluate the Neural Net 5 fold CV
+    
+
+main()
